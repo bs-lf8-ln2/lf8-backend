9f7b5353dd811c50cad2efcf3991fe9f50b15a60
Simon Elias Vincent <elias.simon@ssi-schaefer.com>
2025-01-24 14:08:59 +0100

    HSP-49 create GET endpoint

--- src/main/java/de/szut/lf8_starter/employee/EmployeeController.java
+++ src/main/java/de/szut/lf8_starter/employee/EmployeeController.java
@@ -2,12 +2,12 @@ package de.szut.lf8_starter.employee;
 
 import de.szut.lf8_starter.employee.dto.EmployeeCreateDto;
 import de.szut.lf8_starter.employee.dto.EmployeeGetDto;
+import de.szut.lf8_starter.project.ProjectEntity;
+import de.szut.lf8_starter.project.ProjectMapper;
+import de.szut.lf8_starter.project.dto.ProjectGetDto;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +16,12 @@ import java.util.stream.Collectors;
 public class EmployeeController implements EmployeeControllerOpenAPI {
     private final EmployeeService service;
     private final EmployeeMapper employeeMapper;
+    private final ProjectMapper projectMapper;
 
-    public EmployeeController(EmployeeService service, EmployeeMapper employeeMapper) {
+    public EmployeeController(EmployeeService service, EmployeeMapper employeeMapper, ProjectMapper projectMapper) {
         this.service = service;
         this.employeeMapper = employeeMapper;
+        this.projectMapper = projectMapper;
     }
 
     @PostMapping
@@ -28,6 +30,7 @@ public class EmployeeController implements EmployeeControllerOpenAPI {
         employeeEntity = this.service.create(employeeEntity);
         return this.employeeMapper.mapToGetDto(employeeEntity);
     }
+
     // TODO why not response Entities?
     @GetMapping("/{id}")
     public EmployeeGetDto getById(@PathVariable final Long id) {
@@ -38,12 +41,26 @@ public class EmployeeController implements EmployeeControllerOpenAPI {
             return this.employeeMapper.mapToGetDto(employeeEntity);
         }
     }
+
     @GetMapping
     public List<EmployeeGetDto> findAll() {
         return this.service
                 .readAll()
                 .stream()
                 .map(e -> this.employeeMapper.mapToGetDto(e))
+                .collect(Collectors.toList());
+    }
+
+    // Get all projects of an employee
+    @GetMapping("/{id}/projects")
+    public List<ProjectGetDto> getProjects(@PathVariable final Long id) {
+        // check if employee exists
+        if (this.service.getEmployeeById(id) == null) {
+            return null;
+        }
+        return this.service.getProjects(id)
+                .stream()
+                .map(p -> this.projectMapper.mapToGetDto((ProjectEntity) p))
                 .collect(Collectors.toList());
     }
 }
